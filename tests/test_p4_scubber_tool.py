04aa9abba47093e22b1ba135dfdcdc19d4334ce7
Ryan Maffesoli <pairorat@gmail.com>
2025-08-12 15:28:26 -0400

    exe rebuild

--- tests/test_p4_scubber_tool.py
+++ tests/test_p4_scubber_tool.py
@@ -17,9 +17,10 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import pytest
+from p4_scrubber.p4_scrubber_tool import main
 from collections import namedtuple
 
-args_tuple = namedtuple('ArgsTuple', ['config', 'template_dir'])
+args_tuple = namedtuple('ArgsTuple', ['config', 'manifest', 'yes'])
 
 class MockArgumentParser(object):
     def __init__(self, args_dict=None):
@@ -34,52 +35,60 @@ class MockArgumentParser(object):
         return self.args_dict
 
 
-# @pytest.mark.parametrize(
-#     "given_args,config_values,dir_exists_values",
-#     [
-#         (args_tuple('config', 'template_dir'), {'template_dir': 'template_dir'}, [True, True]),
-#         (args_tuple('config', 'template_dir'), {'template_dir': ''}, [True, True]),
-#         (args_tuple('config', 'template_dir'), {'template_dir': 'template_dir'}, [True, False]),
-#         (args_tuple('config', 'template_dir'), {'template_dir': 'template_dir'}, [False, False]),
-#     ],
-# )
-# def test_main(mocker, given_args, config_values, dir_exists_values):
-#     m_ArgumentParser = mocker.patch("p4templates.p4templates_tool.ArgumentParser", return_value=MockArgumentParser(given_args))
-#     m_os_path_exists = mocker.patch("p4templates.p4templates_tool.os.path.exists", side_effect=dir_exists_values)
-#     m_os_path_abspath = mocker.patch("p4templates.p4templates_tool.os.path.abspath", return_value='abs_path')
-#     m_load_server_config = mocker.patch("p4templates.p4templates_tool.load_server_config", return_value=config_values)
-#     m_os_path_dirname = mocker.patch("p4templates.p4templates_tool.os.path.dirname", return_value='a/dir/name/')
-#     m_os_chdir = mocker.patch("p4templates.p4templates_tool.os.chdir")
-#     m_QApplication = mocker.patch("p4templates.p4templates_tool.QApplication")
-#     m_P4TemplateLoaderDialog = mocker.patch("p4templates.p4templates_tool.P4TemplateLoaderDialog")
+@pytest.mark.parametrize(
+    "given_args,config_values,dir_exists_values,frozen",
+    [
+        (args_tuple('config', 'manifest', 'yes'), {'server': {'port':'test'}}, [True, True], False),
+        (args_tuple('config', 'manifest', 'yes'), {'server': {'port':'test'}}, [True, True], True),
+        (args_tuple('config', 'manifest', 'yes'), {'server': {'port':'test'}}, [True, False], False),
+        (args_tuple('config', 'manifest', 'yes'), {'server': {'port':'test'}}, [False, False], False),
+    ],
+)
+def test_main(mocker, given_args, config_values, dir_exists_values, frozen):
+    m_ArgumentParser = mocker.patch("p4_scrubber.p4_scrubber_tool.ArgumentParser", return_value=MockArgumentParser(given_args))
+    m_os_path_exists = mocker.patch("p4_scrubber.p4_scrubber_tool.os.path.exists", side_effect=dir_exists_values)
+    m_os_path_abspath = mocker.patch("p4_scrubber.p4_scrubber_tool.os.path.abspath", return_value='abs_path')
+    m_load_server_config = mocker.patch("p4_scrubber.p4_scrubber_tool.load_server_config", return_value=config_values)
+    m_read_json = mocker.patch("p4_scrubber.p4_scrubber_tool.read_json", return_value={'manifest': 'manifest'})
+    m_write_json = mocker.patch("p4_scrubber.p4_scrubber_tool.write_json", return_value={'manifest': 'manifest'})
+    m_os_path_dirname = mocker.patch("p4_scrubber.p4_scrubber_tool.os.path.dirname", return_value='a/dir/name/')
+    m_os_chdir = mocker.patch("p4_scrubber.p4_scrubber_tool.os.chdir")
+    m_run_scrubber = mocker.patch("p4_scrubber.p4_scrubber_tool.run_scrubber", return_value={'updated_manifest': 'updated_manifest'})
+    m_setup_server_connection = mocker.patch("p4_scrubber.p4_scrubber_tool.setup_server_connection", return_value='p4_connection')
+    if frozen:
+        mocker.patch("p4_scrubber.p4_scrubber_tool.getattr", return_value=True)
 
-#     main()
+    main()
 
-#     path_exists_calls = [
-#         mocker.call('config'),
-#         mocker.call('template_dir')
-#     ]
+    path_exists_calls = [
+        mocker.call('abs_path'),
+        mocker.call('abs_path')
+    ]
 
-#     abspath_calls = [
-#         mocker.call('config'),
-#         mocker.call('template_dir')
-#     ]
+    abspath_calls = [
+        mocker.call('config'),
+        mocker.call('manifest')
+    ]
 
-#     path_dirname_calls = [
-#         mocker.call('nope')
-#     ]
+    read_json_calls = [
+        mocker.call('config'),
+        mocker.call('manifest')
+    ]
 
-#     m_ArgumentParser.assert_called_once()
-#     m_os_path_exists.assert_has_calls(path_exists_calls)
-#     if dir_exists_values[0] and dir_exists_values[1]:
-#         m_os_path_abspath.assert_has_calls(abspath_calls)
-#         m_load_server_config.assert_called_once_with('abs_path')
-#     m_os_path_dirname.assert_called_once()
-#     m_os_chdir.assert_called_once()
-#     m_QApplication.assert_called_once()
-#     if dir_exists_values[0] and dir_exists_values[1]:
-#         m_P4TemplateLoaderDialog.assert_called_once_with(config_path='abs_path', template_dir='abs_path')
-#     elif dir_exists_values[0]:
-#         m_P4TemplateLoaderDialog.assert_called_once_with(config_path='abs_path', template_dir='template_dir')
-#     else:
-#         m_P4TemplateLoaderDialog.assert_called_once_with(config_path='config', template_dir='template_dir')
+    m_ArgumentParser.assert_called_once()
+    m_os_path_abspath.assert_has_calls(abspath_calls, any_order=True)
+    if dir_exists_values[0] and dir_exists_values[1]:
+        m_os_path_exists.assert_has_calls(path_exists_calls, any_order=True)
+        m_load_server_config.assert_called_once_with('abs_path')
+        m_setup_server_connection.assert_called_once_with(port='test')
+        m_read_json.asset_has_calls(read_json_calls, any_order=True)
+        m_write_json.assert_called_once_with({'updated_manifest': 'updated_manifest'}, 'abs_path')
+        
+        m_run_scrubber.assert_called_once_with('p4_connection', {'manifest':'manifest'}, 0)
+    elif not dir_exists_values[0] or dir_exists_values[1]:
+        m_os_path_exists.assert_called_once_with('abs_path')
+        m_load_server_config.assert_not_called()
+    
+    m_os_path_dirname.assert_called()
+    m_os_chdir.assert_called_once()
+
